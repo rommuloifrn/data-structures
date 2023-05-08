db8c38e32b4bae0b047c0250d37ea805b8c14cb9
Rômulo da Silva Cavalcanti <20221014040015@ifrn.local>
2023-05-08 13:52:07 -0300

    circular array implemented (i think)

--- basicqueue/Queue.java
+++ basicqueue/Queue.java
@@ -22,30 +22,47 @@ public class Queue {
 				System.out.printf("%s, ", arrai[i]);
 			}
 		}
-		System.out.printf("], tamanho %d ", size());
-		if (isEmpty()) System.out.printf("e está vazia%n"); else System.out.printf("%n");
+		System.out.printf("], tamanho %d,", size());
+		if (isEmpty()) System.out.printf(" está vazia"); //else System.out.printf("%n");
+		System.out.printf(" e first é %s (índice %d)", first(), start);
+		System.out.println();
 	}
 	
 	public void enqueue(Object x) {
 		if (this.size() == size-1) {
-			System.out.println("DEBUGA ESSE RAIO");
 			Object aux[] = new Object[size*2];
-			for (int i=0; i<size; i++) {
+			for (int i=0; i<=end; i++) {
 				aux[i] = arrai[i];
 			}
+			if (end < start) {
+				for (int i=start; i<size; i++) {
+					aux[i+size] = arrai[i];
+				}
+				start +=size;
+			}
+			/*
+			for (int i=0; i<size; i++) {
+				aux[i] = arrai[i];
+			}*/
+			
 			arrai = aux;
 			size *=2;
 		}
 		// TEM QUE PASSAR UM NOVO START DUMA FORMA MELHOR AAHAHAHAHA
 		// TEM QUE LEMBRAR QUE O START MUDA DE LUGAR (A LINHA ABAIXO SERVE PRA JOGAR O FIM DA FILA PRO INÍCIO PRA FAZER O ARRAY CIRCULAR)
-		// situação: o array circula com elementos dequeados (o start já não é mais no início do arr). Como prosseguir?
-		if (end == size) end = start;
+		// situação: o array começa a circular quando elementos são dequeados (o start já não é mais no início do arr). Como prosseguir?
+		
+		
+		if (end == size && 0 != start) end = 0; // se o end bater no final do array e o start não for o índice zero, o end vai pra lá.
 		arrai[end] = x;
 		end++;
 	}
 	
 	public Object dequeue() {
-		start++;
+		if (start+1==size) start = 0; else {
+			start++;
+		}
+		System.out.printf("dequeued: %s%n", arrai[end-1]);
 		return arrai[end-1];
 	}
 	
